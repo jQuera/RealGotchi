c38fda3345bfd4c4434e44a071e6c54c2237531e
rafael2425 <rafael2425@gmail.com>
2024-08-11 03:44:18 +0000

    stream con tareas de tasks

--- lib/ui/views/home/widgets/tasks/pages/tasks_bar.dart
+++ lib/ui/views/home/widgets/tasks/pages/tasks_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/ui/common/repository/tasks_repository.dart';
 import 'package:myapp/ui/views/home/widgets/tasks/modal/create_task_bottom_modal.dart';
 
 class TasksBar extends StatelessWidget {
@@ -24,60 +25,135 @@ class TasksBar extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlue.shade300,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  padding: const EdgeInsets.all(12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Felicidad",
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                      Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Row(
-                                children: [
-                                  Checkbox(
-                                    value: false,
-                                    onChanged: (value) {},
-                                  ),
-                                  Text("Jugar con pelota"),
-                                ],
-                              ),
-                              Text("20:15")
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Checkbox(
-                                value: true,
-                                onChanged: (value) {},
-                              ),
-                              Text("Acariciar pelo"),
-                            ],
-                          )
-                        ],
-                      )
-                    ],
-                  ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    StreamBuilder<List<Task>>(
+                        stream: TasksRepository.instance.tasksSaludStream,
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) return const SizedBox.shrink();
+                          return TasksContainer(
+                            categoryTitle: TaskType.salud.name,
+                            tasks: snapshot.data!,
+                          );
+                        }),
+                    const SizedBox(height: 15),
+                    StreamBuilder(
+                        stream: TasksRepository.instance.tasksAlimentacionStream,
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) return const SizedBox.shrink();
+                          return TasksContainer(
+                            categoryTitle: TaskType.alimentacion.name,
+                            tasks: snapshot.data!,
+                          );
+                        }),
+                    const SizedBox(height: 15),
+                    StreamBuilder(
+                        stream: TasksRepository.instance.tasksHigieneStream,
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) return const SizedBox.shrink();
+                          return TasksContainer(
+                            categoryTitle: TaskType.higiene.name,
+                            tasks: snapshot.data!,
+                          );
+                        }),
+                    const SizedBox(height: 15),
+                    StreamBuilder(
+                        stream: TasksRepository.instance.tasksEntrenenimientoStream,
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) return const SizedBox.shrink();
+                          return TasksContainer(
+                            categoryTitle: TaskType.entrenenimiento.name,
+                            tasks: snapshot.data!,
+                          );
+                        }),
+                  ],
                 ),
-              ],
+              ),
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TaskWidget extends StatelessWidget {
+  const TaskWidget({
+    super.key,
+    required this.description,
+    required this.time,
+    required this.completed,
+  });
+
+  final String description;
+  final String time;
+  final bool completed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Checkbox(
+              value: false,
+              onChanged: (value) {},
+            ),
+            Text("Jugar con pelota"),
+          ],
+        ),
+        Text("20:15")
+      ],
+    );
+  }
+}
+
+class TasksContainer extends StatelessWidget {
+  const TasksContainer({
+    super.key,
+    required this.categoryTitle,
+    required this.tasks,
+  });
+
+  final String categoryTitle;
+  final List<Task> tasks;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.lightBlue.shade300,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            categoryTitle,
+            style: Theme.of(context).textTheme.headlineSmall,
           ),
+          tasks.isEmpty
+              ? const Text("Todavia no has asignado tareas")
+              : ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index) {
+                    var task = tasks[index];
+                    return TaskWidget(
+                      description: task.description,
+                      time: task.hour,
+                      completed: task.isCompleted,
+                    );
+                  },
+                )
         ],
       ),
     );
